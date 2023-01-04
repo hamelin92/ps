5eda5b242881b7cdf3101d4f6ae98ebda2b12922
Jang Jinse <78893671+hamelin92@users.noreply.github.com>
2023-01-04 19:07:31 +0900

    [level 4] Title: 무지의 먹방 라이브, Time: 145.08 ms, Memory: 50.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv4/42891. 무지의 먹방 라이브/무지의 먹방 라이브.py
@@ -0,0 +1,15 @@
+def solution(food_times, k):
+    removed = [0]*len(food_times)
+    enumd = list(enumerate(food_times, start=0))
+    enum = sorted(enumd, key=lambda x: x[1], reverse=True)
+    const = 0
+    while enum and (enum[-1][1]-const)*len(enum) <= k:
+        last = enum.pop()
+        k -= (last[1]-const)*(len(enum)+1)
+        removed[last[0]] = 1
+        const = last[1]
+    if not enum:
+        return -1
+    ind = k%len(enum)
+    filtered = list(filter(lambda x: removed[x] == 0,list(range(len(removed)))))
+    return filtered[ind]+1
