a00761dac0085e59f7711b96399fbb9b955842e2
Jang Jinse <78893671+hamelin92@users.noreply.github.com>
2023-01-26 22:14:53 +0900

    [Silver IV] Title: 카드2, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,14 +1,5 @@
-import sys
-sys.setrecursionlimit(10000)
-
-def card_remain(arr):
-    l = len(arr)
-    if l <= 2:
-        return arr[-1]
-    if l%2:
-        return card_remain([arr[-1]] + arr[1::2])
-    return card_remain(arr[1::2])
-    
-
 N = int(input())
-print(card_remain(list(range(1, N+1))))
+if N == 1:
+    print(1)
+else:
+    print((N-(1<<((N-1).bit_length()-1)))*2)
