97451b1110ef820335d1533e68b3c8d043d24ece
Jang Jinse <78893671+hamelin92@users.noreply.github.com>
2023-03-15 14:52:58 +0900

    [Silver IV] Title: 동전 0, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -0,0 +1,8 @@
+N, K = map(int, input().split())
+A = [int(input()) for _ in range(N)]
+ans = 0
+for i in range(N-1, -1, -1):
+    if K >= A[i]:
+        ans += K//A[i]
+        K %= A[i]
+print(ans)  
