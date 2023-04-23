59a6edc0b242f00baefe18973c65b40f0753c0f3
Jang Jinse <78893671+hamelin92@users.noreply.github.com>
2023-04-24 02:23:10 +0900

    [Silver III] Title: 조합, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2407. 조합/조합.py
+++ 백준/Silver/2407. 조합/조합.py
@@ -1,7 +1,7 @@
 
 N, K = map(int, input().split())
 K = min(K, N-K)
-C = [[1]*n for n in range(1, N+2)]
+C = [[1]*min(n, K+1) for n in range(1, N+2)]
 for i in range(1, N+1):
     for k in range(max(i-N+K, 1), min(i, K+1)):
         C[i][k] = C[i-1][k-1] + C[i-1][k]
