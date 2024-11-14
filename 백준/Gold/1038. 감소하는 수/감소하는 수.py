ebe37ac94421f8d8b1ccb3047a0f269f23e66996
Jang Jinse <78893671+hamelin92@users.noreply.github.com>
2024-11-14 22:32:30 +0900

    [Gold V] Title: 감소하는 수, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1038. 감소하는 수/감소하는 수.py
+++ 백준/Gold/1038. 감소하는 수/감소하는 수.py
@@ -1,36 +1,29 @@
-from math import comb
-
-
-def combination(arr, r):
-
-	def generate(chosen):
-		global N_cop, result
-		if len(chosen) == r:
-			N_cop += 1
-			if N_cop == comb(10, r)+1:
-				result = chosen[:]
-			return
-
-		start = arr.index(chosen[-1]) + 1 if chosen else 0
-		for nxt in range(start, len(arr)):
-			chosen.append(arr[nxt])
-			generate(chosen)
-			chosen.pop()
-	generate([])
-
+from itertools import combinations
 
 N = int(input())
-if N > 2**10 - 2:
-	print(-1)
+
+# n자리 감소하는 수는 9876543210 에서 서로 다른 숫자 n개를 선택하면 결정된다.
+nums = [0]*11 # 리스트의 i번째 원소가 i자리 이하의 감소하는 수의 갯수를 나타내도록 한다.
+nums[0] = 1
+
+for i in range(1, 11):
+    nums[i] = (nums[i-1]*(11-i))//i
+
+if N > 1022:
+    print(-1)
 else:
-	N_cop = N + 1
-	n = 1
-	while N_cop > comb(10,n):
-		N_cop -= comb(10,n)
-		n += 1
-	result = []
-	combination(list(range(9,-1,-1)), n)
-	answer = 0
-	for i in range(n):
-		answer += result[n-i-1] * 10**i
-	print(answer)
+    r = 0 # 자리수
+    k = N+1 # 같은 자리 수 내에서의 순서
+    for i in range(11):
+        if k >= nums[i]:
+            k = k-nums[i]
+            r = i
+        else:
+            if k >= 0:
+                r += 1
+            break
+    choice = list(combinations(range(9,-1,-1), r))[-k-1]
+    ans = ""
+    for i in range(r):
+        ans += str(choice[i])
+    print(ans)
