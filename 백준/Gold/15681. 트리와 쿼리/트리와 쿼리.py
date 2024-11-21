12134b7b52d30655c6185fa000163d2bf7639a80
Jang Jinse <78893671+hamelin92@users.noreply.github.com>
2024-11-21 22:25:09 +0900

    [Gold V] Title: 트리와 쿼리, Time: 476 ms, Memory: 77636 KB -BaekjoonHub

--- 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.py
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.py
@@ -5,17 +5,14 @@ sys.setrecursionlimit(1000000)
 
 N, R, Q = map(int, sys.stdin.readline().split())
 subnodes = [1]*(N+1)
-tree = defaultdict(set)
+tree = defaultdict(list)
 for _ in range(1, N):
     u, v = map(int, sys.stdin.readline().split())
-    tree[u].add(v)
-    tree[v].add(u)
+    tree[u].append(v)
+    tree[v].append(u)
 sub = [int(sys.stdin.readline()) for _ in range(Q)]
 
 def count_node(parent, node):
-    # if parent in tree[node] and len(tree[node]) == 1:
-    #     # subnodes[node] += 1
-    #     return 1
     for child in tree[node]:
         if child != parent:
             subnodes[node] += count_node(node, child)
