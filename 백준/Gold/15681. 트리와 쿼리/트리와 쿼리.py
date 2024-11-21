d54ef02893fc18013dc9d8e048663a52fcbc614f
Jang Jinse <78893671+hamelin92@users.noreply.github.com>
2024-11-21 22:22:29 +0900

    [Gold V] Title: 트리와 쿼리, Time: 560 ms, Memory: 92996 KB -BaekjoonHub

--- 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.py
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.py
@@ -2,6 +2,7 @@ from collections import defaultdict
 import sys
 
 sys.setrecursionlimit(1000000)
+
 N, R, Q = map(int, sys.stdin.readline().split())
 subnodes = [1]*(N+1)
 tree = defaultdict(set)
@@ -10,23 +11,17 @@ for _ in range(1, N):
     tree[u].add(v)
     tree[v].add(u)
 sub = [int(sys.stdin.readline()) for _ in range(Q)]
-visit = [0]*(N+1)
 
-def remove_parent(parent, node):
-    if parent in tree[node]:
-        tree[node].remove(parent)
+def count_node(parent, node):
+    # if parent in tree[node] and len(tree[node]) == 1:
+    #     # subnodes[node] += 1
+    #     return 1
     for child in tree[node]:
-        remove_parent(node, child)
-
-remove_parent(-1, R)
-
-def count_node(node):
-    if visit[node]:
-        return subnodes[node]
-    visit[node] = 1
-    for child in tree[node]:
-        subnodes[node] += count_node(child)
+        if child != parent:
+            subnodes[node] += count_node(node, child)
     return subnodes[node]
 
+count_node(-1, R)
+
 for s in sub:
-    sys.stdout.write(f'{count_node(s)}\n')
+    sys.stdout.write(f'{subnodes[s]}\n')
