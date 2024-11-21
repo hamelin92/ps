9567f7d021ce28c5292a258dcfa731d395d91b86
Jang Jinse <78893671+hamelin92@users.noreply.github.com>
2024-11-21 22:29:49 +0900

    [Gold V] Title: 트리와 쿼리, Time: 396 ms, Memory: 68844 KB -BaekjoonHub

--- 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.py
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.py
@@ -10,7 +10,7 @@ for _ in range(1, N):
     u, v = map(int, sys.stdin.readline().split())
     tree[u].append(v)
     tree[v].append(u)
-sub = [int(sys.stdin.readline()) for _ in range(Q)]
+# sub = [int(sys.stdin.readline()) for _ in range(Q)]
 
 def count_node(parent, node):
     for child in tree[node]:
@@ -20,5 +20,5 @@ def count_node(parent, node):
 
 count_node(-1, R)
 
-for s in sub:
-    sys.stdout.write(f'{subnodes[s]}\n')
+for _ in range(Q):
+    sys.stdout.write(f'{subnodes[int(sys.stdin.readline())]}\n')
