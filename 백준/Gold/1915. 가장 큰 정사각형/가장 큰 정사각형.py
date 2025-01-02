4ea6afde436a1a0f72fc115d461b180b0447e5e1
Jang Jinse <78893671+hamelin92@users.noreply.github.com>
2025-01-02 11:05:22 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 728 ms, Memory: 57308 KB -BaekjoonHub

--- 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.py
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.py
@@ -1,9 +1,18 @@
+import sys
+input = sys.stdin.readline
 
 n, m = map(int, input().split())
-nums = [[0]*(m+1)] + [[0]+[int(num) for num in input()] for _ in range(n)]
+nums = [list(map(int, list(input().rstrip("\n")))) for _ in range(n)]
 max_sq = 0
-for i in range(1, n+1):
-    for j in range(1, m+1):
+for i in range(n):
+    if nums[i][0] > max_sq:
+        max_sq = 1
+if max_sq == 0:
+    for j in range(m):
+        if nums[0][j] > max_sq:
+            max_sq = 1
+for i in range(1, n):
+    for j in range(1, m):
         if nums[i][j] == 1:
             nums[i][j] = min(nums[i-1][j-1], nums[i][j-1], nums[i-1][j]) + 1
             if nums[i][j] > max_sq:
