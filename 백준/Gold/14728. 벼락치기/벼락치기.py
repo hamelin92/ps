768c28b80c070fa70c2c3b8a79795862b421cd5a
Jang Jinse <78893671+hamelin92@users.noreply.github.com>
2025-04-03 22:18:10 +0900

    [Gold V] Title: 벼락치기, Time: 472 ms, Memory: 54804 KB -BaekjoonHub

--- 백준/Gold/14728. 벼락치기/벼락치기.py
+++ 백준/Gold/14728. 벼락치기/벼락치기.py
@@ -3,13 +3,13 @@ import sys
 input = lambda: sys.stdin.readline().rstrip()
 
 N, T = map(int, input().split())
-part = [[0,0]] + [list(map(int, input().split())) for _ in range(N)]
 dp = [[0]*(T+1) for _ in range(N+1)]
 
 for i in range(1,N+1):
+    K, S = map(int, input().split())
     for j in range(1, T+1):
-        if j >= part[i][0]:
-            dp[i][j] = max(dp[i-1][j], dp[i-1][j-part[i][0]] + part[i][1])
+        if j >= K:
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-K] + S)
         else:
             dp[i][j] = dp[i-1][j]
 print(max(dp[-1]))
