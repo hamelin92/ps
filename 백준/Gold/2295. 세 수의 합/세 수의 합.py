ae0dce2b12a221ff121b6035d80b750a1a3bd564
Jang Jinse <78893671+hamelin92@users.noreply.github.com>
2025-04-10 21:29:43 +0900

    [Gold IV] Title: 세 수의 합, Time: 488 ms, Memory: 67232 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.py
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.py
@@ -6,13 +6,12 @@ N = int(input())
 U = [int(input()) for _ in range(N)]
 U.sort()
 
-sums = set(U)
+ans = 0
+sums = set()
 for x in U:
     for y in U:
         d = x+y
-        if d < U[-1]:
-            sums.add(d)
-ans = 0
+        sums.add(d)
 
 for k in range(N-1, -1, -1):
     for z in range(k):
