15dd1328e748eda0a1f941a4d8e2da76f66544c0
Jang Jinse <78893671+hamelin92@users.noreply.github.com>
2025-07-31 21:16:21 +0900

    [Gold IV] Title: 팰린드롬?, Time: 2228 ms, Memory: 63888 KB -BaekjoonHub

---
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.py
@@ -0,0 +1,24 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+
+N = int(input())
+nums = list(map(int, input().split()))
+M = int(input())
+dp = [[0]*N for _ in range(N)]
+
+for i in range(N):
+    dp[i][i] = 1
+for i in range(N-1):
+     if nums[i] == nums[i+1]:
+        dp[i][i+1] = 1
+
+for l in range(2, N):
+    for s in range(N-l):
+        e = s+l
+        if nums[s] == nums[e] and dp[s+1][e-1]:
+                dp[s][e] = 1
+
+for _ in range(M):
+    s, e = map(int, input().split())
+    print(dp[s-1][e-1])
